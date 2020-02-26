ffaf6ac12ac888dafc8efec4b80b5269f3622416
Max Karpushonok <max.karpushonok@gmail.com>
2020-02-26 14:54:47 +0300

    Created js function registration()

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function registration() {
+    var signIn = document.getElementById('signin');
+    signIn.style.display = 'none';
+    var registration = document.getElementById('registration')
+    registration.style.display = 'block';
+}
